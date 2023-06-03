8fa50120b83655e7cc3b71b2b2e8ef1907098e9b
Maxson Ferovante <maxsonferovante@gmail.com>
2023-06-03 18:26:18 -0300

    heroku
    heroku

---
+++ build/precache-manifest.de96229164271f74e4c2b6253546b5e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "26e68eee37bf72e1ee44",
+    "url": "/static/css/main.b3a04d6d.chunk.css"
+  },
+  {
+    "revision": "26e68eee37bf72e1ee44",
+    "url": "/static/js/main.26e68eee.chunk.js"
+  },
+  {
+    "revision": "58294a3d856a0542909d",
+    "url": "/static/js/1.58294a3d.chunk.js"
+  },
+  {
+    "revision": "b939e03187932d068264",
+    "url": "/static/css/2.b9d16104.chunk.css"
+  },
+  {
+    "revision": "b939e03187932d068264",
+    "url": "/static/js/2.b939e031.chunk.js"
+  },
+  {
+    "revision": "4594604607cbe964e03e",
+    "url": "/static/js/runtime~main.45946046.js"
+  },
+  {
+    "revision": "29e8843bed2b329463c6d47f675075b3",
+    "url": "/index.html"
+  }
+];
